e5c611a6418d1266402865a4690b4c3992d45d71
ricardojusto <ricardojusto@outlook.com>
2015-07-17 17:09:35 +0100

    created people table

---
+++ db/migrate/20150717155936_create_people_table.rb
@@ -0,0 +1,13 @@
+class CreatePeopleTable < ActiveRecord::Migration
+  def change
+  	create_table :people do |table|
+  		table.string :email, unique: true, null: false
+  		table.string :name, null: false
+  		table.string :password, null: false
+  		table.string :sex
+  		table.date :birth_date
+  		table.string :qualification
+  		table.string :nationality
+  		table.boolean :is_teacher, null: false, default: false
+  end
+end
