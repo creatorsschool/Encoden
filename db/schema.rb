3645750bcc75300193659227b4dc49d522e48cd2
rumama <rumama@users.noreply.github.com>
2015-07-23 12:15:29 +0100

    add add javascript, css and fonts dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723091127) do
+ActiveRecord::Schema.define(version: 20150723093816) do
+
+  create_table "payments", force: :cascade do |t|
+    t.date     "payment_date",                 null: false
+    t.boolean  "paid",         default: false
+    t.integer  "course_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                      null: false
