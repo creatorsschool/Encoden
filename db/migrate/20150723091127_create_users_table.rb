67271b216f69eec95377de4acb3fc95ae03a51ea
rumama <rumama@users.noreply.github.com>
2015-07-23 10:21:45 +0100

    Update users table
    Drop old table and create a new db with new migration

--- db/migrate/20150723091127_create_users_table.rb
+++ db/migrate/20150723091127_create_users_table.rb
@@ -2,7 +2,6 @@ class CreateUsersTable < ActiveRecord::Migration
 	def change
 		create_table :users do |table|
 			table.string :email, unique: true, null: false
-			table.string :name, null: false
 			table.boolean :teacher, null: false, default: false
 			table.integer :user_id
 		end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20150723091127) do
 
   create_table "users", force: :cascade do |t|
     t.string  "email",                   null: false
-    t.string  "name",                    null: false
     t.boolean "teacher", default: false, null: false
     t.integer "user_id"
   end
