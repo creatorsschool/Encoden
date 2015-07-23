5ffc5b7f4a491d446590e331cd5f2911d12107ed
ricardojusto <ricardojusto@outlook.com>
2015-07-23 12:22:42 +0100

    created courses table

--- db/migrate/20150723111149_create_courses_table.rb
+++ db/migrate/20150723111149_create_courses_table.rb
@@ -1,4 +1,11 @@
 class CreateCoursesTable < ActiveRecord::Migration
   def change
+  	create_table :courses do |table|
+  		table.string :name
+  		table.text :description
+  		table.time :duration
+  		table.float :price
+  		table.timestamps
+  	end
   end
 end
