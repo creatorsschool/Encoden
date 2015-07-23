08a7661c0510c3525da6a28470226b4c2cb38810
rumama <rumama@users.noreply.github.com>
2015-07-23 14:38:50 +0100

    add chapters to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723111149) do
+ActiveRecord::Schema.define(version: 20150723133239) do
+
+  create_table "chapters", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
