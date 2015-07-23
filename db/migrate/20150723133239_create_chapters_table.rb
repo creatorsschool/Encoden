c3c11ae71542303bf446eedcaf29438cf552b498
rumama <rumama@users.noreply.github.com>
2015-07-23 14:37:23 +0100

    create chapters table

--- db/migrate/20150723133239_create_chapters_table.rb
+++ db/migrate/20150723133239_create_chapters_table.rb
@@ -1,4 +1,9 @@
 class CreateChaptersTable < ActiveRecord::Migration
   def change
+  	create_table :chapters do |table|
+			table.string :name
+			table.text :description
+			table.timestamps
+		end
   end
 end
