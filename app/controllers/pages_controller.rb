190be41cdcd513b70fdf02eaff34cc67aae75018
rumama <rumama@users.noreply.github.com>
2015-07-31 15:50:39 +0100

    add courses controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,7 @@
 class PagesController < ApplicationController
 
-	# clearance
-	# only require login in dashboad
-	before_action :require_login, only: [:dashboard]
-
 	def index
 
-	end
-
-
-	def dashboard
-		render layout:"admin"
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,4 @@ Rails.application.routes.draw do
 	resources :courses
 	root to: 'pages#index'
 
-
-	get '/dashboard', to: 'pages#dashboard'
-
 end
