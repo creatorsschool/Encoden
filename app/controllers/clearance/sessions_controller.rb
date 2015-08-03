736eded790c3ec5f509042b692f3298e7c46a328
Bruno Brito <brunofbrito@gmail.com>
2015-08-03 10:54:43 +0100

    Adds simple flash after login

--- app/controllers/clearance/sessions_controller.rb
+++ app/controllers/clearance/sessions_controller.rb
@@ -8,6 +8,7 @@ class Clearance::SessionsController < Clearance::BaseController
 
     sign_in(@user) do |status|
       if status.success?
+        flash[:notice] = "You have logged in successfully!"
         redirect_back_or url_after_create
       else
         flash.now.notice = status.failure_message
