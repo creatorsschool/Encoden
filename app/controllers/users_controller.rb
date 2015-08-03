81be5ac6dfacd44e3073b3da02369dee8e300670
Bruno Brito <brunofbrito@gmail.com>
2015-08-03 10:15:33 +0100

    Added Edit User option

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.update(params[:id], user_params)
+    redirect_to courses_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 	resources :courses
+  resources :users
 	root to: 'pages#index'
 
 end
