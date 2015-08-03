f1ade4e37a13a658d186e2ab997c8900cbd405b5
rumama <rumama@users.noreply.github.com>
2015-08-03 11:19:40 +0100

    add lessons to courses views

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,28 @@
+class LessonsController < ApplicationController
+
+
+	def show
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def new
+		@lesson = Lesson.new
+	end
+
+	def create
+
+	end
+
+	def destroy
+
+	end
+
+end
+
