16beb6d534403a7cbc1626c589a7194745093fe8
ricardojusto <ricardojusto@outlook.com>
2015-08-04 15:13:07 +0100

    submit button accompanies the creation of more input fields for creating more chapters

--- app/assets/javascripts/our.js
+++ app/assets/javascripts/our.js
@@ -8,18 +8,22 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   var max_fields      = 10; //maximum input boxes allowed
-  var inputs         = $("#input-field"); //Fields wrapper
-  var wrapper = $("#chapter-form");
+  var inputs          = $("#input-field"); //Fields wrapper
+  var wrapper         = $("#chapter-form");
   var add_button      = $(".add_field_button"); //Add button ID
+  var x               = 1;
+  var form_btn        = $("#form-btn");
 
-  var x = 1;
   $(add_button).on("click", function(e){
     console.log("HELOO");
     e.preventDefault();
     if(x < max_fields){
       x++;
       $(wrapper).append(inputs.clone());
-
     }
   });
 });
+
+
+
+
