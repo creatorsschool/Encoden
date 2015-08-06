6c11c9f6b848beff3cec9b45baec8a6dccda660a
Bruno Brito <brunofbrito@gmail.com>
2015-08-06 13:27:04 +0100

    Adds 2 decimal places and € currency

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,11 @@
 class Course < ActiveRecord::Base
+  include ActionView::Helpers::NumberHelper
 	validates_presence_of :name, :description, :duration, :price
 	has_many :payments
   has_many :chapters, dependent: :destroy
 	belongs_to :user
+
+  def formatted_price
+    number_to_currency(price, precision: 2, unit: '€', format: "%n %u")
+  end
 end
