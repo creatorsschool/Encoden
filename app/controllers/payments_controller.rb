b1be8cc69256b47f04047818dbd96dedb18e9868
ricardojusto <ricardojusto@outlook.com>
2015-08-10 16:39:11 +0100

    add condition for buy to avoid user buy the same course again

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,21 +9,19 @@ def new
 end
 
   def create
-    course = Course.find(params[:course_id])
-      result = Braintree::Transaction.sale(
-        :amount => course.price,
-        :payment_method_nonce => params[:payment_method_nonce],
-        :options => {
-          :submit_for_settlement => true
-        }
-        )
+    @course = Course.find(params[:course_id])
+    result = Braintree::Transaction.sale(
+      :amount => @course.price,
+      :payment_method_nonce => params[:payment_method_nonce],
+      :options => {
+        :submit_for_settlement => true
+      }
+    )
 
     if result.success?
-      @course = Course.find(params[:course_id])
       @course.update(paid: true)
       redirect_to courses_path, notice: "Congratulations! Your transaction has been completed successfully!"
     else
-      @course = Course.find(params[:course_id])
       flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
       @client_token = Braintree::ClientToken.generate
       render :new
