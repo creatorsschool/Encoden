d6e935ede28c678c88596b9824919437fb30766d
ricardojusto <ricardojusto@outlook.com>
2015-08-10 13:01:05 +0100

    create result transaction in Payment Controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,13 +1,20 @@
 class PaymentsController < ApplicationController
-  require 'braintree'
-
   def new
     @course = Course.find(params[:course_id])
     @client_token = Braintree::ClientToken.generate
   end
 
   def create
-
+    course = Course.find(params[:course_id])
+    result = Braintree::Transaction.sale(
+      :amount => course.price,
+      :payment_method_nonce => params[:payment_method_nonce],
+      :options => {
+        :submit_for_settlement => true
+      }
+    )
+    p result
+    redirect_to courses_path
   end
 
 end
--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,20 +1,4 @@
-require "rubygems"
-require "braintree"
-
 Braintree::Configuration.environment = :sandbox
 Braintree::Configuration.merchant_id = 'tzdcz4khr3gnmpgv'
 Braintree::Configuration.public_key = 'dmtqzp7rtys22pt4'
 Braintree::Configuration.private_key = 'c4c61fb3734eee0f0e227613e172749e'
-Braintree::Test::Nonce::Transactable
-Braintree::Test::Nonce::Consumed
-Braintree::Test::Nonce::PayPalOneTimePayment
-Braintree::Test::Nonce::PayPalFuturePayment
-
-post "/payment-methods" do
-  nonce = params[:payment_method_nonce]
-end
-
-result = Braintree::Transaction.sale(
-  :amount => "100.00",
-  :payment_method_nonce => nonce_from_the_client
-)
