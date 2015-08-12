840f8137b36e3fea7fe5db5ea959f9bbaca7e5b1
Bruno Brito <brunofbrito@gmail.com>
2015-08-12 18:02:03 +0100

    Fixed destroy attachment route

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,5 +1,19 @@
 class ResourcesController < ApplicationController
-  
+
+ def index
+    @resources = Resource.all
+  end
+
+ def create
+    @resource = Resource.new(file_name: params[:file])
+    if @resource.save!
+      respond_to do |format|
+        format.json{ render :json => @resource }
+      end
+    end
+  end
+
+
   def destroy
     resource = Resource.find(params[:id])
     resource.destroy
