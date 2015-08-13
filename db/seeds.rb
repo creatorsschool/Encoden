53940d9178bdb750c57834869d1bb6850e42ef17
ricardojusto <ricardojusto@outlook.com>
2015-08-13 23:03:10 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,10 @@ Lesson.destroy_all
 User.create(name: "admin", email: 'admin@gmail.com', password: 'teste', teacher: true)
 User.create(name: "user", email: 'user@gmail.com', password: 'teste', teacher: false)
 
-Course.create(name: "Web Development", description: "This is first Course", duration: 10, price: 200, user_id: 1, paid: false)
+Course.create(name: "Web Development", description: "This is first Course", duration: 10, price: 110, user_id: 1, paid: false)
 Chapter.create(name: "Introduction to websites", description: "First Chapter from course 1", course_id: 1)
 Lesson.create(name: "Education, understanding an implementation", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 1)
-Lesson.create(name: "Image and file sizes explained ", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 1)
+Lesson.create(name: "Image and file sizes explained ", description: "Lesson 2 from Chapter 1 and Course 1", chapter_id: 1)
 Lesson.create(name: "How to resize images correctly Part 1", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 1)
 Lesson.create(name: "How to resize images correctly Part 2", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 1)
 Lesson.create(name: "How Web sites work", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 1)
@@ -25,7 +25,7 @@ Lesson.create(name: "Downloading files for the Web site", description: "Lesson 1
 
 Chapter.create(name: "Autoresponders", description: "First Chapter from course 1", course_id: 1)
 Lesson.create(name: "How email works", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 2)
-Lesson.create(name: "Working with autoresponders", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 2)
+Lesson.create(name: "Working with autoresponders", description: "Lesson 2 from Chapter 1 and Course 1", chapter_id: 2)
 Lesson.create(name: "Email Marketing", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 2)
 Lesson.create(name: "Spamming 101", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 2)
 Lesson.create(name: "Spamming 102", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 2)
@@ -47,7 +47,7 @@ Lesson.create(name: "Spamming 101", description: "Lesson 1 from Chapter 1 and Co
 Lesson.create(name: "Spamming 102", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 4)
 Lesson.create(name: "Email Analytics", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 4)
 
-Course.create(name: "Blogging 101", description: "This is second Course", duration: 10, price: 200, user_id: 1, paid: false)
+Course.create(name: "Blogging 101", description: "This is second Course", duration: 10, price: 99,99, user_id: 1, paid: false)
 Chapter.create(name: "Blogging", description: "First Chapter from course 1", course_id: 2)
 Lesson.create(name: "How email works", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 5)
 Lesson.create(name: "Working with autoresponders", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 5)
@@ -57,9 +57,9 @@ Lesson.create(name: "Spamming 102", description: "Lesson 1 from Chapter 1 and Co
 Lesson.create(name: "Email Analytics", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 5)
 
 
-Course.create(name: "Servers", description: "This is second Course", duration: 10, price: 200, user_id: 1, paid: false)
+Course.create(name: "Servers", description: "This is second Course", duration: 10, price: 35, user_id: 1, paid: false)
 Chapter.create(name: "Blogging", description: "First Chapter from course 1", course_id: 3)
-Lesson.create(name: "How email works", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 5)
+Lesson.create(name: "How email works", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 6)
 Lesson.create(name: "Working with autoresponders", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 6)
 Lesson.create(name: "Email Marketing", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 6)
 Lesson.create(name: "Spamming 101", description: "Lesson 1 from Chapter 1 and Course 1", chapter_id: 6)
